96f2e27a5360774b6212002f7f5f6b58158bdd52
RambhuSingh1819 <156609631+RambhuSingh1819@users.noreply.github.com>
2026-02-15 09:50:48 +0530

    Time: 28 ms (39.58%) | Memory: 49.8 MB (88.43%) - LeetSync

--- 697-degree-of-an-array/degree-of-an-array.java
+++ 697-degree-of-an-array/degree-of-an-array.java
@@ -1,36 +1,31 @@
 class Solution {
     public int findShortestSubArray(int[] nums) {
 
-        // value -> [firstIndex, lastIndex, frequency]
-        HashMap<Integer, int[]> map = new HashMap<>();
+        HashMap<Integer,Integer> freq = new HashMap<>();
+        HashMap<Integer,Integer> first = new HashMap<>();
+        HashMap<Integer,Integer> last = new HashMap<>();
+        
+        int maxFreq = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            int x = nums[i];
+        for(int i = 0; i < nums.length; i++){
+            int num = nums[i];
+            freq.put(num,freq.getOrDefault(num,0)+1);
 
-            if (!map.containsKey(x)) {
-                map.put(x, new int[]{i, i, 1});
-            } else {
-                map.get(x)[1] = i;      // update last index
-                map.get(x)[2]++;        // increase frequency
+            if(!first.containsKey(num)){
+                first.put(num,i);
+            }
+            last.put(num,i);
+
+            maxFreq = Math.max(maxFreq,freq.get(num));
+
+        }
+        int minLen = nums.length;
+        for(int ele : freq.keySet()){
+            if(freq.get(ele) == maxFreq){
+                minLen = Math.min(minLen,last.get(ele) - first.get(ele) + 1);
             }
         }
-
-        int degree = 0;
-        int ans = nums.length;
-
-        for (int[] data : map.values()) {
-            int first = data[0];
-            int last = data[1];
-            int freq = data[2];
-
-            if (freq > degree) {
-                degree = freq;
-                ans = last - first + 1;
-            } else if (freq == degree) {
-                ans = Math.min(ans, last - first + 1);
-            }
-        }
-
-        return ans;
+        return minLen;
+        
     }
 }
