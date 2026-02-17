b5e67b4d66eb3b695a1fe85550f48d99012cf039
RambhuSingh1819 <156609631+RambhuSingh1819@users.noreply.github.com>
2026-02-17 08:41:47 +0530

    Time: 5 ms (97.77%) | Memory: 44 MB (58.70%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,20 +1,33 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if(s.length() != t.length()) return false;
-        HashMap<Character,Character> map = new HashMap<>();
-        Set<Character> set = new HashSet<>();
-        for(int i = 0; i < s.length(); i++){
-            char c1 = s.charAt(i);
-            char c2 = t.charAt(i);
+        // if(s.length() != t.length()) return false;
+        // HashMap<Character,Character> map = new HashMap<>();
+        // Set<Character> set = new HashSet<>();
+        // for(int i = 0; i < s.length(); i++){
+        //     char c1 = s.charAt(i);
+        //     char c2 = t.charAt(i);
 
-            if(map.containsKey(c1)){
-                if(map.get(c1) != c2) return false;
-            }else {
-                if(set.contains(c2)) return false;
-                map.put(c1,c2);
-                set.add(c2);
-            }
+        //     if(map.containsKey(c1)){
+        //         if(map.get(c1) != c2) return false;
+        //     }else {
+        //         if(set.contains(c2)) return false;
+        //         map.put(c1,c2);
+        //         set.add(c2);
+        //     }
 
+        // }
+        // return true;
+
+        int[] strs= new int[256];
+        int[] strt= new int[256];
+        int n= s.length();
+        if(n!= t.length()) return false;
+        for(int i=0; i<n; i++){
+            char a= s.charAt(i);
+            char b= t.charAt(i);
+            if(strs[a]!= strt[b]) return false;
+            strs[a]= i+1;
+            strt[b]= i+1;
         }
         return true;
     }
