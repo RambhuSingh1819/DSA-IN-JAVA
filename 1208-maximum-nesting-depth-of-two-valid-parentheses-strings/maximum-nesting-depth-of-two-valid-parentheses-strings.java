9ade1e156fc2a7a029b39a64c6f161c6bdd41962
RambhuSingh1819 <156609631+RambhuSingh1819@users.noreply.github.com>
2026-02-20 06:53:00 +0530

    Time: 1 ms (100.00%) | Memory: 45.5 MB (45.11%) - LeetSync

--- 1208-maximum-nesting-depth-of-two-valid-parentheses-strings/maximum-nesting-depth-of-two-valid-parentheses-strings.java
+++ 1208-maximum-nesting-depth-of-two-valid-parentheses-strings/maximum-nesting-depth-of-two-valid-parentheses-strings.java
@@ -1,14 +1,22 @@
 class Solution {
     public int[] maxDepthAfterSplit(String seq) {
+        int n = seq.length();
         int[] arr = new int[seq.length()];
         int cnt = 0;
-        for(int i = 0; i < seq.length(); i++){
-            if(seq.charAt(i) == '('){
-                cnt++;
-                arr[i] = cnt%2;
-            }else {
-                arr[i] = cnt%2;
-                cnt--;
+        // for(int i = 0; i < seq.length(); i++){
+        //     if(seq.charAt(i) == '('){
+        //         cnt++;
+        //         arr[i] = cnt%2;
+        //     }else {
+        //         arr[i] = cnt%2;
+        //         cnt--;
+        //     }
+        // }
+        for (int i = 0, x = 0; i < n; ++i) {
+            if (seq.charAt(i) == '(') {
+                arr[i] = x++ & 1;
+            } else {
+                arr[i] = --x & 1;
             }
         }
         return arr;
