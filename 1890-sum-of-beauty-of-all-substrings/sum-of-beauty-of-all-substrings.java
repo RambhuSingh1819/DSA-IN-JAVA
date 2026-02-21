76f7e770204675e49bc9851e026dd604d724f443
RambhuSingh1819 <156609631+RambhuSingh1819@users.noreply.github.com>
2026-02-21 08:41:58 +0530

    Time: 60 ms (77.64%) | Memory: 44.4 MB (37.23%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -1,28 +1,26 @@
 class Solution {
     public int beautySum(String s) {
-        int cnt = 0;
-        for(int i = 0; i < s.length(); i++){
-            for(int j = i; j <s.length(); j++){
-                String  str = s.substring(i,j+1);
-                cnt += findFreq(str);
-            }   
-        }
-        return cnt;
-    }
-    public int findFreq(String s){
-        int[] arr = new int[26];
-        for(int i = 0; i < s.length(); i++){
-            arr[s.charAt(i)-'a']++;
-        }
-        int maxi = Integer.MIN_VALUE;
-        int mini = Integer.MAX_VALUE;
-       for(int i = 0; i < 26; i++){
-        if(arr[i] > 0){
-            maxi = Math.max(arr[i],maxi);
-            mini = Math.min(arr[i],mini);
-        }
-       }
-       return maxi-mini;
+        int n = s.length();
+        int res = 0;
 
+        for (int i = 0; i < n; i++) {
+            int[] freq = new int[26];
+            for (int j = i; j < n; j++) {
+                freq[s.charAt(j) - 'a']++;
+
+                int maxFreq = 0;
+                int minFreq = Integer.MAX_VALUE;
+
+                for (int f : freq) {
+                    if (f > 0) {
+                        maxFreq = Math.max(maxFreq, f);
+                        minFreq = Math.min(minFreq, f);
+                    }
+                }
+
+                res += (maxFreq - minFreq);
+            }
+        }
+        return res;
     }
 }
