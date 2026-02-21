be7382e937cba95b87bf40dde9cfa8835cf3e6a0
RambhuSingh1819 <156609631+RambhuSingh1819@users.noreply.github.com>
2026-02-21 22:14:04 +0530

    Time: 0 ms (100.00%) | Memory: 61.4 MB (41.15%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,37 +1,18 @@
-// import java.util.Arrays;
-
-// class Solution {
-//     public void rotate(int[] nums, int k) {
-//         int len = nums.length;
-//         if (len == 0 || k % len == 0) return;
-//         k = k % len;
-//         int[] temp = new int[k];
-//         for (int i = 0; i < k; i++) {
-//             temp[i] = nums[len - k + i];
-//         }
-//         for (int i = len - k - 1; i >= 0; i--) {
-//             nums[i + k] = nums[i];
-//         }
-//         for (int i = 0; i < k; i++) {
-//             nums[i] = temp[i];
-//         }
-//         System.out.print(Arrays.toString(nums) + " ");
-//     }
-// }
 class Solution {
     public void rotate(int[] nums, int k) {
-        k = k % nums.length; // handle k > n
-        reverse(nums, 0, nums.length - 1);
-        reverse(nums, 0, k - 1);
-        reverse(nums, k, nums.length - 1);
+        int n= nums.length;
+        k = k % n;
+        reverse(nums,0,n-k-1);
+        reverse(nums,n-k,n-1);
+        reverse(nums,0,n-1);
+        return ;
     }
-
-    private void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            int temp = nums[start];
-            nums[start] = nums[end];
+    public void reverse(int[] nums,int st , int end){
+        while(st < end){
+            int temp = nums[st];
+            nums[st] = nums[end];
             nums[end] = temp;
-            start++;
+            st++;
             end--;
         }
     }
