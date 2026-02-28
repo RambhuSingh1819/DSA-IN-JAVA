45a48611a71765c5d08a2cc8685ddf07aab60e58
RambhuSingh1819 <156609631+RambhuSingh1819@users.noreply.github.com>
2026-03-01 00:35:33 +0530

    Time: 23 ms (91.44%) | Memory: 81.4 MB (33.33%) - LeetSync

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minimizedMaximum(int n, int[] quantities) {
+
+        int low = 1;
+        int high = 0;
+        for(int ele : quantities){
+           high = Math.max(ele,high);
+        }
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(isPossible(n,quantities,mid)){
+                high = mid-1;
+            }else low = mid + 1;
+        }
+        return low;
+    }
+    public boolean isPossible(int n , int[] nums, int mid){
+        int sum = 0;
+        for(int i : nums){
+            sum += (i+mid-1)/mid;
+            if(sum > n) return false;
+        }
+        return true;
+    }
+}
