373d6ec6cf3098a75411e56731001ea1de682c7d
Rambhu Singh <156609631+RambhuSingh1819@users.noreply.github.com>
2026-03-18 06:50:09 +0530

    Time: 2 ms (99.53%) | Memory: 45.1 MB (73.64%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -8,9 +8,8 @@ class Solution {
     }
     public void solution(int [] nums, int idx, List<List<Integer>> list, List<Integer> ans){
        list.add(new ArrayList<>(ans));
-
         for (int i = idx; i < nums.length; i++) {
-            if (i > idx && nums[i] == nums[i - 1]) continue;
+            if (i != idx && nums[i] == nums[i - 1]) continue;
         ans.add(nums[i]);
         solution(nums,i+1,list,ans);
         ans.remove(ans.size()-1);
