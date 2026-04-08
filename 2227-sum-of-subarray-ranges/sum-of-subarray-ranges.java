e13020be0ec4f1107a2d4b82f6e42518897648ff
Rambhu Singh <156609631+RambhuSingh1819@users.noreply.github.com>
2026-04-08 10:44:54 +0530

    Time: 16 ms (83.23%) | Memory: 46.5 MB (55.54%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -1,80 +1,100 @@
-import java.util.*;
+// import java.util.*;
 
+// class Solution {
+//     public long subArrayRanges(int[] nums) {
+//         int n = nums.length;
+
+//         long maxSum = getMaxSum(nums);
+//         long minSum = getMinSum(nums);
+
+//         return maxSum - minSum;
+//     }
+//     private long getMaxSum(int[] nums) {
+//         int n = nums.length;
+//         int[] pge = new int[n]; 
+//         int[] nge = new int[n]; 
+
+//         Stack<Integer> stk = new Stack<>();
+
+//         for (int i = 0; i < n; i++) {
+//             while (!stk.isEmpty() && nums[stk.peek()] < nums[i]) {
+//                 stk.pop();
+//             }
+//             pge[i] = stk.isEmpty() ? -1 : stk.peek();
+//             stk.push(i);
+//         }
+
+//         stk.clear();
+
+//         for (int i = n - 1; i >= 0; i--) {
+//             while (!stk.isEmpty() && nums[stk.peek()] <= nums[i]) {
+//                 stk.pop();
+//             }
+//             nge[i] = stk.isEmpty() ? n : stk.peek();
+//             stk.push(i);
+//         }
+
+//         long sum = 0;
+
+//         for (int i = 0; i < n; i++) {
+//             long left = i - pge[i];
+//             long right = nge[i] - i;
+//             sum += left * right * nums[i];
+//         }
+
+//         return sum;
+//     }
+//     private long getMinSum(int[] nums) {
+//         int n = nums.length;
+//         int[] pse = new int[n]; 
+//         int[] nse = new int[n]; 
+
+//         Stack<Integer> stk = new Stack<>();
+//         for (int i = 0; i < n; i++) {
+//             while (!stk.isEmpty() && nums[stk.peek()] > nums[i]) {
+//                 stk.pop();
+//             }
+//             pse[i] = stk.isEmpty() ? -1 : stk.peek();
+//             stk.push(i);
+//         }
+
+//         stk.clear();
+//         for (int i = n - 1; i >= 0; i--) {
+//             while (!stk.isEmpty() && nums[stk.peek()] >= nums[i]) {
+//                 stk.pop();
+//             }
+//             nse[i] = stk.isEmpty() ? n : stk.peek();
+//             stk.push(i);
+//         }
+
+//         long sum = 0;
+
+//         for (int i = 0; i < n; i++) {
+//             long left = i - pse[i];
+//             long right = nse[i] - i;
+//             sum += left * right * nums[i];
+//         }
+
+//         return sum;
+//     }
+// }
 class Solution {
     public long subArrayRanges(int[] nums) {
-        int n = nums.length;
+        int l = nums.length;
+       long sum =0;
+        for(int i=0;i<l;i++)
+        {
+            int smallest = nums[i];
+            int largest = nums[i];
+            for(int j=i;j<l;j++)
+            {
+                largest = Math.max(largest,nums[j]);
+                smallest = Math.min(smallest,nums[j]);
+                sum = sum + (largest - smallest);
 
-        long maxSum = getMaxSum(nums);
-        long minSum = getMinSum(nums);
-
-        return maxSum - minSum;
-    }
-    private long getMaxSum(int[] nums) {
-        int n = nums.length;
-        int[] pge = new int[n]; 
-        int[] nge = new int[n]; 
-
-        Stack<Integer> stk = new Stack<>();
-
-        for (int i = 0; i < n; i++) {
-            while (!stk.isEmpty() && nums[stk.peek()] < nums[i]) {
-                stk.pop();
             }
-            pge[i] = stk.isEmpty() ? -1 : stk.peek();
-            stk.push(i);
         }
-
-        stk.clear();
-
-        for (int i = n - 1; i >= 0; i--) {
-            while (!stk.isEmpty() && nums[stk.peek()] <= nums[i]) {
-                stk.pop();
-            }
-            nge[i] = stk.isEmpty() ? n : stk.peek();
-            stk.push(i);
-        }
-
-        long sum = 0;
-
-        for (int i = 0; i < n; i++) {
-            long left = i - pge[i];
-            long right = nge[i] - i;
-            sum += left * right * nums[i];
-        }
-
         return sum;
-    }
-    private long getMinSum(int[] nums) {
-        int n = nums.length;
-        int[] pse = new int[n]; 
-        int[] nse = new int[n]; 
-
-        Stack<Integer> stk = new Stack<>();
-        for (int i = 0; i < n; i++) {
-            while (!stk.isEmpty() && nums[stk.peek()] > nums[i]) {
-                stk.pop();
-            }
-            pse[i] = stk.isEmpty() ? -1 : stk.peek();
-            stk.push(i);
-        }
-
-        stk.clear();
-        for (int i = n - 1; i >= 0; i--) {
-            while (!stk.isEmpty() && nums[stk.peek()] >= nums[i]) {
-                stk.pop();
-            }
-            nse[i] = stk.isEmpty() ? n : stk.peek();
-            stk.push(i);
-        }
-
-        long sum = 0;
-
-        for (int i = 0; i < n; i++) {
-            long left = i - pse[i];
-            long right = nse[i] - i;
-            sum += left * right * nums[i];
-        }
-
-        return sum;
+        
     }
 }
