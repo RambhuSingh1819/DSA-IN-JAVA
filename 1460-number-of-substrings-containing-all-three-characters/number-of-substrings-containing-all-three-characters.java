5f87502488a3a7689dd46b9b9a6536b826c244ac
Rambhu Singh <156609631+RambhuSingh1819@users.noreply.github.com>
2026-04-15 22:35:05 +0530

    Time: 13 ms (72.07%) | Memory: 46.1 MB (81.71%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -4,12 +4,11 @@ class Solution {
         int cnt = 0;
         for(int i = 0; i < s.length(); i++){
             char ch = s.charAt(i);
-           ans[ch-'a'] = i;
-           if(ans[0] != -1 && ans[1] != -1 && ans[2] != -1){
-            cnt = cnt + (1+Math.min(ans[0],Math.min(ans[1],ans[2])));
-           }
+            ans[ch-'a'] = i;
+            if(ans[0] != -1 && ans[1] != -1 && ans[2] != -1){
+                 cnt = cnt + (1+Math.min(ans[0],Math.min(ans[1],ans[2])));
+            }
         }
         return cnt;
-         
     }
 }
