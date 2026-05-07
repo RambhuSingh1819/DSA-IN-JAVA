3b91270f559156c1cad306a74386b9e1741e1841
Rambhu Singh <156609631+RambhuSingh1819@users.noreply.github.com>
2026-05-07 09:25:16 +0530

    Time: 31 ms (72.76%) | Memory: 48.1 MB (37.28%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -2,34 +2,57 @@ class Solution {
 
     public boolean isNStraightHand(int[] hand, int groupSize) {
 
-        if (hand.length % groupSize != 0) {
+        // if (hand.length % groupSize != 0) {
+        //     return false;
+        // }
+        // TreeMap<Integer, Integer> map = new TreeMap<>();
+        // for (int ele : hand) {
+        //     map.put(ele, map.getOrDefault(ele, 0) + 1);
+        // }
+
+        // while (!map.isEmpty()) {
+
+        //     int fEle = map.firstKey();
+
+        //     for (int i = 0; i < groupSize; i++) {
+
+        //         int nEle = fEle + i;
+
+        //         if (!map.containsKey(nEle)) {
+        //             return false;
+        //         }
+
+        //         map.put(nEle, map.get(nEle) - 1);
+
+        //         if (map.get(nEle) == 0) {
+        //             map.remove(nEle);
+        //         }
+        //     }
+        // }
+
+        // return true;
+        if(hand.length%groupSize!=0){
             return false;
         }
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        for (int ele : hand) {
-            map.put(ele, map.getOrDefault(ele, 0) + 1);
+
+        HashMap<Integer,Integer> maps = new HashMap<>();
+        for(int map : hand){
+            maps.put(map,maps.getOrDefault(map,0)+1);
         }
+        Arrays.sort(hand);
+        for(int num : hand){
+            if(maps.get(num) == 0){
+                continue;
+            }
+            for(int i = 0;i<groupSize;i++){
+                int current = num+i;
 
-        while (!map.isEmpty()) {
-
-            int fEle = map.firstKey();
-
-            for (int i = 0; i < groupSize; i++) {
-
-                int nEle = fEle + i;
-
-                if (!map.containsKey(nEle)) {
+                if(!maps.containsKey(current) || maps.get(current) == 0){
                     return false;
                 }
-
-                map.put(nEle, map.get(nEle) - 1);
-
-                if (map.get(nEle) == 0) {
-                    map.remove(nEle);
-                }
+                maps.put(current,maps.get(current)-1);
             }
         }
-
         return true;
     }
 }
