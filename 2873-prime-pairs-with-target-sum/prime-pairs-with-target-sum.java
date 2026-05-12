fbd073a0b73cb5e6099dfef29983bfc5729e1f3d
Rambhu Singh <156609631+RambhuSingh1819@users.noreply.github.com>
2026-05-12 19:33:34 +0530

    Time: 117 ms (95.36%) | Memory: 53.9 MB (75.68%) - LeetSync

--- 2873-prime-pairs-with-target-sum/prime-pairs-with-target-sum.java
+++ 2873-prime-pairs-with-target-sum/prime-pairs-with-target-sum.java
@@ -1,30 +1,23 @@
 class Solution {
-    Set<Integer> set = new HashSet<>();
     public List<List<Integer>> findPrimePairs(int n) {
         List<List<Integer>> list = new ArrayList<>();
-        isPrime(n);
+
+        if(n < 4) return list;
+
+        boolean[] check = new boolean[n+1];
+        check[0] = check[1] = true;
+
+        for(int i = 2; i * i < n; i++){
+           if(!check[i]) {
+            for(int j = i * i; j < n; j += i) check[j] = true;
+           }
+        }
+        
         for(int i = 0; i <= n / 2; i++){
-            if(set.contains(i) && set.contains(n-i))
-            list.add(Arrays.asList(i,n-i));
+            int y = n - i;
+            if(!check[i] && !check[y]) list.add(Arrays.asList(i,y));
         }
         return list;
     }
-    public void isPrime(int n){
-        if(n < 2) return;
-        boolean[] check = new boolean[n];
-        Arrays.fill(check,true);
-        check[0] = false;
-        check[1] = false;
-
-        int rem = 0;
-
-        for(int i = 2; i * i < n; i++){
-           if(check[i]) {
-            for(int j = i * i; j < n; j += i) check[j] = false;
-           }
-        }
-        for(int i = 0; i < check.length; i++){
-            if(check[i]) set.add(i);
-        }
-    }
+   
 }
