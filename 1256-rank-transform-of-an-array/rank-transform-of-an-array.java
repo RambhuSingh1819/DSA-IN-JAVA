cc7fd7a8ad4443ebed22d86e7698522745335ebd
Rambhu Singh <156609631+RambhuSingh1819@users.noreply.github.com>
2026-07-12 09:52:06 +0530

    Time: 31 ms (59.74%) | Memory: 76.9 MB (38.30%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -12,7 +12,7 @@ class Solution {
             }
         }
         for(int i = 0; i < arr.length; i++){
-            if(map.containsKey(arr[i])) arr[i] = map.get(arr[i]);
+            arr[i] = map.get(arr[i]);
         }
         return arr;
         
