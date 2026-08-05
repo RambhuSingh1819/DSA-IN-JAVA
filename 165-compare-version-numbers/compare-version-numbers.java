1cd282e50ebb11bd243a1c3bbc0d8acb79654f50
Rambhu Singh <156609631+RambhuSingh1819@users.noreply.github.com>
2026-08-05 11:33:20 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (88.92%) - LeetSync

---
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        int i = 0, j = 0;
+        int n1 = version1.length(), n2 = version2.length();
+        while(i < n1 || j < n2){
+            int NO1 = 0;
+            int NO2 = 0;
+            while(i < n1 && version1.charAt(i) != '.'){
+                NO1 = NO1 * 10 + (version1.charAt(i)-'0');
+                i++;
+            }
+            i++;
+            while(j < n2 && version2.charAt(j) != '.'){
+                NO2 = NO2 * 10 + (version2.charAt(j)-'0');
+                j++;
+            }
+            j++;
+
+            if(NO1 < NO2) return -1;
+            else if(NO1 > NO2) return 1;
+        }
+        return 0;
+    }
+}
