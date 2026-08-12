6e62c3880a2e85ad7375bf2a9e286464f75b5add
Rambhu Singh <156609631+RambhuSingh1819@users.noreply.github.com>
2026-08-13 00:28:39 +0530

    Time: 1 ms (22.79%) | Memory: 42.2 MB (82.91%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.java
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -1,21 +1,22 @@
 class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
         int[] arr = {1,2,3,4,5,6,7,8,9};
-        List<List<Integer>> out = new ArrayList<>();
-        solve(arr,out,0,new ArrayList<>(),k,n);
-        return out;
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        if(n == 0 || k > n) return ans;
+        solve(0,arr,k,n,ans,new ArrayList<>());
+        return ans;
     }
-     public void solve(int[] arr,List<List<Integer>> out,int index,List<Integer> ans, int k,int n){
+    public void solve(int idx, int[] arr, int k , int n ,List<List<Integer>> ans, List<Integer> temp){
         if(k == 0 && n == 0){
-            out.add(new ArrayList(ans));
+            ans.add(new ArrayList<>(temp));
             return;
         }
-        if(k < 0 || n < 0) return;
-        for(int i = index; i < arr.length; i++){
+        for(int i = idx; i < arr.length;i++){
             if(arr[i] > n) break;
-            ans.add(arr[i]);
-            solve(arr,out,i+1,ans,k-1,n-arr[i]);
-            ans.remove(ans.size()-1);
+            temp.add(arr[i]);
+            solve(i+1,arr,k-1,n-arr[i],ans,temp);
+            temp.remove(temp.size()-1);
         }
     }
 }
