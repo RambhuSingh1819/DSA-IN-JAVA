4dec3c3410a5bbaf536e3c7e426e9b45e639294f
Rambhu Singh <156609631+RambhuSingh1819@users.noreply.github.com>
2026-08-17 07:13:35 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (48.23%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
         for(int[] row : dp) Arrays.fill(row,-1);
@@ -13,5 +13,24 @@ class Solution {
         int left = solve(m,n-1,dp);
 
         return dp[m][n] = left + up;
+    }
+} */
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 && j == 0) dp[i][j] = 1;
+                else {
+                    int right = 0;
+                    int down = 0;
+                    if(j > 0) right = dp[i][j-1];
+                    if(i > 0) down = dp[i-1][j];
+                    dp[i][j] = down + right;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
