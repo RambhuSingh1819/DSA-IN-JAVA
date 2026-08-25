332a2c5535eaa31fe9b40bd1a0e9a1317495dc34
Rambhu Singh <156609631+RambhuSingh1819@users.noreply.github.com>
2026-08-25 11:36:31 +0530

    Time: 15 ms (94.49%) | Memory: 58.6 MB (17.70%) - LeetSync

--- 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.java
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.java
@@ -1,26 +1,26 @@
 class Solution {
     public long repairCars(int[] ranks, int cars) {
         long low = 1;
-        long max = Integer.MAX_VALUE;
-        for(int i : ranks) max = Math.min(max,i);
-        long  high = max*cars*cars;
+        long max = Long.MAX_VALUE;
+        for(int ele : ranks) max= Math.min(max,ele);
+        long high = max *(long) cars * cars;
         long ans = high;
         while(low <= high){
-            long mid = low + (high - low) / 2;
-            if(isPossibel(ranks,cars,mid)){
+            long mid = low + (high - low)/2;
+            if(isPossible(ranks,mid,cars)){
                 ans = mid;
-                high = mid-1;
+                high = mid -1;
             }else low = mid + 1;
         }
-
         return ans;
     }
-    public boolean isPossibel(int[] nums, int n, long mid){
-        long total = 0;
-        for(int ele : nums){
-            total += Math.sqrt(mid/ele);
-            if(total >= n) return true;
+    public boolean isPossible(int[] ranks,long mid, int k){
+        long num = 0;
+        for(int it : ranks){
+            //n = sqrt(mid/r)
+            num +=(long) Math.sqrt(mid/it);
+            if(num >= k) return true;
         }
-        return false;
+        return false;   
     }
 }
