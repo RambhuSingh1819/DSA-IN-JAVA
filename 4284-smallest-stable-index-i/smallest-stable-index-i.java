ece08702f079eed8363a2c0a248fcec24b9efb42
Rambhu Singh <156609631+RambhuSingh1819@users.noreply.github.com>
2026-09-04 10:08:47 +0530

    Time: 1 ms (99.79%) | Memory: 46.1 MB (88.30%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.java
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.java
@@ -4,17 +4,21 @@ class Solution {
         int[] min = new int[n];
         min[n-1] = nums[n-1];
         for(int i = n-2; i >= 0;i--){
+            //FIND ALL THE MIN VALUE FROM LAST AND STORE IT 
             min[i] = Math.min(nums[i],min[i+1]);
         }
         int max = nums[0];
         for(int i = 1; i < n;i++){
+            //FIND THE MAX VALUE FROM FROM STARTING AND STORE IT
             max = Math.max(nums[i],max);
             nums[i] = max;
         }
         for(int i = 0; i < n; i++){
+            //IF BASE CONDITION MEET RETURN
             int val = nums[i] - min[i];
             if(val <= k) return i;
         }
+        // IF NO MIN VALUE THEN K THEN RETURN -1
         return -1;
     }
 }
