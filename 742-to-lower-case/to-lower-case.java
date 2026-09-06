6454f61e56d5c417ffbe3084e45ec21c097a979f
Rambhu Singh <156609631+RambhuSingh1819@users.noreply.github.com>
2026-09-06 11:04:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (68.12%) - LeetSync

--- 742-to-lower-case/to-lower-case.java
+++ 742-to-lower-case/to-lower-case.java
@@ -1,5 +1,12 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();   
+       // return s.toLowerCase(); 
+       char[] chr = s.toCharArray();
+       for(int i = 0; i < chr.length; i++){
+            if(chr[i] >='A' && chr[i] <='Z'){
+                chr[i] = (char)(chr[i]+32);
+            }
+       }  
+       return new String(chr);
     }
 }
