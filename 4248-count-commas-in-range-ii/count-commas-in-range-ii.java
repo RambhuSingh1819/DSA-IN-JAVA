c4ca075429c1440343623a9db4d264cf3a539f8e
Rambhu Singh <156609631+RambhuSingh1819@users.noreply.github.com>
2026-09-09 21:34:28 +0530

    Time: 1 ms (99.36%) | Memory: 42.7 MB (56.41%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long countCommas(long n) {
+        if(n < 999) return 0;
+        long comma = 1;
+        long low = 1000;
+        long res = 0;
+        while(low <= n){
+            long up = low*1000-1;
+            if(up > n) up = n;
+            long cntNum = up-low+1;
+            res += (cntNum*comma);
+            low *= 1000;
+            comma ++;
+        }
+        return res;
+    }
+}
