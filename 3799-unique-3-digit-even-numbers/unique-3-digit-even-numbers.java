3af1138adac8d28b0ddbec150084b7cbc35dfafb
Rambhu Singh <156609631+RambhuSingh1819@users.noreply.github.com>
2026-09-11 10:24:49 +0530

    Time: 5 ms (82.80%) | Memory: 45.9 MB (75.87%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
@@ -1,24 +1,22 @@
 class Solution {
     public int totalNumbers(int[] digits) {
-        boolean[] used = new boolean[1000];
-        int ans = 0;
-        for (int i = 0; i < digits.length; i++) {
-            if (digits[i] == 0) continue;
-            for (int j = 0; j < digits.length; j++) {
-                if (j == i) continue;
-                for (int k = 0; k < digits.length; k++) {
-                    if (k == i || k == j) continue;
-                    if (digits[k] % 2 != 0) continue;
-                    int num = digits[i] * 100
-                            + digits[j] * 10
-                            + digits[k];
-                    used[num] = true;
+        int n = digits.length;
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0;i < n; i++){
+            for(int j = 0; j < n; j++){
+                for(int k = 0; k <n; k++){
+                    if(i != j && j != k && i != k){
+                        if(digits[i] != 0) {
+                            if(digits[k] % 2 == 0){
+                                int num = digits[i]*100+digits[j]*10+digits[k];
+                                set.add(num);
+                            }
+                        }
+                    }
+                   
                 }
             }
         }
-        for (boolean x : used) {
-            if (x) ans++;
-        }
-        return ans;
+        return set.size();
     }
 }
